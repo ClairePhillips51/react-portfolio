90eba648d4e68772e4e4eec9eb246e1c58db208a
Claire Phillips <claireacam@gmail.com>
2021-06-09 18:59:41 -0400

    set page skeletons and router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
