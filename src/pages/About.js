a31738d99536e7366ff292ccfeda07be12848ca2
Claire Phillips <claireacam@gmail.com>
2021-06-11 19:04:52 -0400

    made edits to the projects page and about me page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,19 @@ import React from "react";
 
 function About() {
     return (
-        <div>About</div>
+        <div>
+        <h2> About Me: </h2>
+        <p>
+            I'm Claire Phillips. I am learning full stack web development with experience in digital marketing and public relations. 
+            I am a digital marketing specialist at Dart Container corporation and am looking to take on the world of web developement. 
+            I recently completed a coding bootcamp through Michigan State Univeristy and am looking for a new adventure.
+            I have experience in both front-end and back-end development. I have a proven knowledge of database development, 
+            web programming, and web platform development. I aiming to leverage my skills to successfully fill the Web Developer role at your company. 
+            This protfolio will showcase some of the projects I've completed. You can find a full list of projects I'm working on or have completed on my 
+            <a href="https://github.com/ClairePhillips51?tab=repositories" target="_blank" rel="noreferrer"> GitHub page </a>
+        </p>
+        </div>
+
     );
 }
 
