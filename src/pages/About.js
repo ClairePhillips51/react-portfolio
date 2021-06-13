90fa924c579b20ba2695ea294cb7801d1bdc0104
Claire Phillips <claireacam@gmail.com>
2021-06-13 14:46:06 -0400

    added spacing and styling to the about me page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import "./about.css";
 
@@ -8,16 +7,20 @@ function About() {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-12 col-lg-4">
-                    <img id="headshot" className="img-fluid" src ="./images/claire2.jpg" alt="A head shot of Claire Phillips" />
-                    <a href="pictures/Claire_Phillips_Resume2.pdf" target="_blank" rel="noreferrer"> My Resume</a>
-                    <p> Connect with me: </p>
-                    <div className="social-links">
-                        <a href="mailto:claireacam@gmail.com" target="_blank" rel="noreferrer"><FontAwesomeIcon title="Email Me: ClairePhillips712@gmail.com" icon={faEnvelope} size="2x" /></a>
-                        <a href="/contact"><FontAwesomeIcon title= "(832)-741-8680" icon={faMobileAlt} size="2x"/></a>
-                        <a href="https://github.com/ClairePhillips51" target="_blank" rel="noreferrer"><FontAwesomeIcon title="GitHub" icon={faGithub} size="2x" /></a>
-                        <a href="https://www.linkedin.com/in/claire-phillips-46baa062/" target="_blank" rel="noreferrer"><FontAwesomeIcon title="LinkedIn" icon={faLinkedin} size="2x" /></a>
-                        <a href="https://www.instagram.com/clairecamerontv/?hl=en" target="_blank" rel="noreferrer"><FontAwesomeIcon title="Instagram" icon={faInstagram} size="2x" /></a>
+                <div id="picLinks" className="col-12 col-lg-4 container">
+                    <div className="row">
+                        <div className="col-6 col-lg-12">
+                            <img id="headshot" src ="./images/claire2.jpg" alt="A head shot of Claire Phillips" />
+                        </div>
+                        <div className="col-6 col-lg-12">
+                            <a className="a" href="pictures/Claire_Phillips_Resume2.pdf" target="_blank" rel="noreferrer">My Resume</a>
+                            <p id="connect"> Connect with me: </p>
+                            <div className="social-links">
+                                <a href="https://github.com/ClairePhillips51" target="_blank" rel="noreferrer"><FontAwesomeIcon title="GitHub" icon={faGithub} size="2x" /></a>
+                                <a href="https://www.linkedin.com/in/claire-phillips-46baa062/" target="_blank" rel="noreferrer"><FontAwesomeIcon title="LinkedIn" icon={faLinkedin} size="2x" /></a>
+                                <a href="https://www.instagram.com/clairecamerontv/?hl=en" target="_blank" rel="noreferrer"><FontAwesomeIcon title="Instagram" icon={faInstagram} size="2x" /></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="col-12 col-lg-8">
@@ -29,8 +32,8 @@ function About() {
                         I have experience in both front-end and back-end development. I have a proven knowledge of database development, 
                         web programming, and web platform development. I aiming to leverage my skills to successfully fill the Web Developer role at your company. 
                         This protfolio will showcase some of the projects I've completed. You can find a full list of projects I'm working on or have
-                        completed on my  <a href="https://github.com/ClairePhillips51?tab=repositories" target="_blank" rel="noreferrer">GitHub page</a>.
-                        You can also find learn more about me in my <a href="pictures/Claire_Phillips_Resume2.pdf" target="_blank" rel="norfererrer">resume.</a>
+                        completed on my <a className="a" href="https://github.com/ClairePhillips51?tab=repositories" target="_blank" rel="noreferrer">GitHub</a>.
+                        You can also find learn more about me in my <a className="a" href="pictures/Claire_Phillips_Resume2.pdf" target="_blank" rel="norfererrer">resume.</a>
                     </p>
                 </div>
             </div>
