3367d858f143fe959b233382cf1b412b8e07b71e
Claire Phillips <claireacam@gmail.com>
2021-06-13 14:06:32 -0400

    Made the navbar responsive/collapsable to screensize

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/js/dist/collapse";
 import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Home
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/about"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About Me
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/projects"
-              className={window.location.pathname === "/projects" ? "nav-link active" : "nav-link"}
-            >
-              My Projects
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/contact"
-              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-            >
-              Contact Me
-            </Link>
-          </li>
-        </ul>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Claire Phillips
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                to="/about"
+                className={
+                  window.location.pathname === "/" || window.location.pathname === "/about"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                About Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/projects"
+                className={window.location.pathname === "/projects" ? "nav-link active" : "nav-link"}
+              >
+                My Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+              >
+                Contact Me
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
