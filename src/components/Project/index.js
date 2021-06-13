fda84cdb4a267dd24c60f7445a34729905efe1f5
Claire Phillips <claireacam@gmail.com>
2021-06-13 15:16:07 -0400

    Started styling projects page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,13 +3,15 @@ import "./style.css";
 
 function Project(props) {
     return (
-        <div className="card col-12 col-lg-6">
-            <img src={props.image} alt={props.title} />
-            <h5 className="card-title">{props.title}</h5>
-            <p className="card-text">{props.text}</p>
-            <div>
-                <a href={props.delpoyed_url} className="btn btn-primary">Deployed Project</a>
-                <a href={props.github_url} className="btn btn-secondary">GitHub Repository</a>
+        <div className="col-12 col-lg-6">
+            <div className="card">
+                <img src={props.image} alt={props.title} />
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.text}</p>
+                <div>
+                    <a href={props.delpoyed_url} className="btn btn-primary">Deployed Project</a>
+                    <a href={props.github_url} className="btn btn-secondary">GitHub Repository</a>
+                </div>
             </div>
         </div>
     );
