5fb9af66a48dcbf6b34482fabbc5a880641fba50
Claire Phillips <claireacam@gmail.com>
2021-06-14 19:35:09 -0400

    Add resume to files

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,7 @@ function About() {
                             <img id="headshot" src ="./images/claire2.jpg" alt="A head shot of Claire Phillips" />
                         </div>
                         <div className="col-6 col-lg-12">
-                            <a className="a" href="pictures/Claire_Phillips_Resume2.pdf" target="_blank" rel="noreferrer">My Resume</a>
+                            <a className="a" href="./images/Claire_Phillips_Resume2.pdf" target="_blank" rel="noreferrer">My Resume</a>
                             <p id="connect">Connect with me:</p>
                             <div className="social-links">
                                 <a href="https://github.com/ClairePhillips51" target="_blank" rel="noreferrer"><FontAwesomeIcon title="GitHub" icon={faGithub} size="2x" style={linkStyle}/></a>
@@ -37,7 +37,7 @@ function About() {
                         web programming, and web platform development. I aiming to leverage my skills to successfully fill the Web Developer role at your company. 
                         This protfolio will showcase some of the projects I've completed. You can find a full list of projects I'm working on or have
                         completed on my <a className="a" href="https://github.com/ClairePhillips51?tab=repositories" target="_blank" rel="noreferrer">GitHub</a>.
-                        You can also find learn more about me in my <a className="a" href="pictures/Claire_Phillips_Resume2.pdf" target="_blank" rel="norfererrer">resume.</a>
+                        You can also find learn more about me in my <a className="a" href="./images/Claire_Phillips_Resume2.pdf" target="_blank" rel="norfererrer">resume.</a>
                     </p>
                 </div>
             </div>
